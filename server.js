f18f0d70a94a86988281cfba4bcd5b55bd46e21a
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-16 16:45:29 -0500

    MVP

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
+server.use(helmet());
 server.use(express.json());
 
 const usersRouter = require('./users/usersRouter.js')
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,10 +1,42 @@
 const express = require('express');
-
+const bcrypt = require('bcryptjs');
+const restricted = require('../middlewere/restricted.js')
 const usersDb = require('./usersHelper.js');
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
+router.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+
+    usersDb.add({username, password: hash })
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+})
+
+router.post('/login', (req, res)=> {
+    let { username, password } = req.body;
+    usersDb.findBy({ username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: `Welcome ${user.username}!` });
+          } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+          }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json(error);
+      });
+})
+
+
+router.get('/users', restricted, (req, res) => {
     usersDb.find()
         .then(user => {
             res.status(200).json(user)
@@ -15,5 +47,6 @@ router.get('/users', (req, res) => {
         })
     
 })
+
 
 module.exports = router;
