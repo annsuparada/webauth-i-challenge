d445a13d5fd2ec99d50e912fa924fb89ab600f19
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-17 14:25:29 -0500

    add cookie

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
-server.use(helmet());
-server.use(express.json());
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session)
 
 const usersRouter = require('./users/usersRouter.js')
+const dbConnection = require('./data/db-config.js')
 
+const sessionConfig = {
+   name: 'chocochip', 
+     secret:'keep it secrect',
+     cookie: {
+       maxAge: 1000 * 60 * 60,
+       secure: false,
+     },
+     httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+     resave: false,
+     saveUninitialized: true,
+     store: new KnexSessionStore({
+       knex: dbConnection,
+       tablename: 'knexsession',
+       sidfielname: 'sessionid',
+       createtable: true,
+       clearInterval: 1000 * 60 * 30,
+     })
+ }
 
-server.get('/', (req, res) => {
-   res.send('<h1>Hello from Express<h1>')
-});
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
 
 server.use('/api', usersRouter);
 
